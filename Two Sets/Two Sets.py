3b7948dba8ba111679685d2f0af01548ea0c7ce6
DxnielRavindran <bumbles0506@gmail.com>
2022-03-07 22:56:48 +0800

    Two sets partial code

---
+++ Two Sets/Two Sets.py
@@ -0,0 +1,18 @@
+
+def TwoSets(n):
+    nope = "NO"
+    yess = "YES"
+    set_1 = []
+    set_2 = []
+    answer = []
+    if n%2 == 0:
+        return nope
+
+    return answer
+         
+
+
+
+    
+
+    
